ebb3968a8bef027dd0106f52c1361e9dfed16284
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-08-07 20:30:24 +0700

    chore: add progress day 20

---
+++ assets/day-20/vite-react-styling/src/components/button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Button.module.css";
+
+type Props = {};
+
+const Button = (props: Props) => {
+  return <button className="buttonComponent">Click Me</button>;
+};
+
+export default Button;
